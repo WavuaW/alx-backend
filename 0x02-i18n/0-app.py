4c5100ad28e4e167fc394a1e07c26ccc7874b86f
WavuaW <wavuamuroka@gmail.com>
2023-08-29 20:36:50 +0300

    0x0 Basic Flask

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+#!/usr/bin/env python3
+from flask import Flask, render_template
 
-app = Flask(__main__)
+app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("0-index.html")
 if __name__ == "__main__":
     app.run()
